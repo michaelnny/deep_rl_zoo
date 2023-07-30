Improve numerical stability for PPO, fix typos in NGU, Agent57

deep_rl_zoo/agent57/agent.py
@@ -128,6 +128,7 @@ class Actor(types_lib.Agent):
         ucb_beta: float,
         ucb_epsilon: float,
         episodic_memory_capacity: int,
+        reset_episodic_memory: bool,
         num_neighbors: int,
         cluster_distance: float,
         kernel_epsilon: float,
@@ -157,6 +158,7 @@ class Actor(types_lib.Agent):
             ucb_beta: beta for the sliding window UCB algorithm.
             ucb_epsilon: exploration epsilon for sliding window UCB algorithm.
             episodic_memory_capacity: maximum capacity of episodic memory.
+            reset_episodic_memory: Reset the episodic_memory on every episode.
             num_neighbors: number of K-NN neighbors for compute episodic bonus.
             cluster_distance: K-NN neighbors cluster distance for compute episodic bonus.
             kernel_epsilon: K-NN kernel epsilon for compute episodic bonus.
@@ -249,6 +251,8 @@ class Actor(types_lib.Agent):
         self._policy_discount = None
         self._sample_policy()
 
+        self._reset_episodic_memory = reset_episodic_memory
+
         # E-greedy policy epsilon, rank 0 has the lowest noise, while rank N-1 has the highest noise.
         epsilons = distributed.get_actor_exploration_epsilon(num_actors)
         self._exploration_epsilon = epsilons[self.rank]
@@ -332,7 +336,8 @@ class Actor(types_lib.Agent):
         """This method should be called at the beginning of every episode before take any action."""
         self._unroll.reset()
 
-        self._episodic_module.reset()  # Is this necessary?
+        if self._reset_episodic_memory:
+            self._episodic_module.reset()
 
         self._update_actor_network(True)
 
@@ -760,7 +765,7 @@ class Learner(types_lib.Learner):
         discount_t = (~done).float() * discount  # (T+1, B)
 
         # Derive target policy probabilities from q values.
-        target_policy_probs = F.softmax(q_t, dim=-1)  # [T+1, B, action_dim]
+        target_policy_probs = F.softmax(target_q_t, dim=-1)  # [T+1, B, action_dim]
 
         if self._transformed_retrace:
             transform_tx_pair = nonlinear_bellman.SIGNED_HYPERBOLIC_PAIR
@@ -773,8 +778,8 @@ class Learner(types_lib.Learner):
             q_t=target_q_t[1:],
             a_tm1=a_t[:-1],
             a_t=a_t[1:],
-            r_t=r_t[:-1],
-            discount_t=discount_t[:-1],
+            r_t=r_t[1:],
+            discount_t=discount_t[1:],
             pi_t=target_policy_probs[1:],
             mu_t=behavior_prob_a_t[1:],
             lambda_=self._retrace_lambda,

deep_rl_zoo/gym_env.py
@@ -221,19 +221,17 @@ class ResizeAndGrayscaleFrame(gym.ObservationWrapper):
             dtype=np.uint8,
         )
 
-    def observation(self, observation):
-        frame = observation
-
-        # pylint: disable=no-member
-        if self.grayscale:
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        frame = cv2.resize(frame, (self.frame_width, self.frame_height), interpolation=cv2.INTER_AREA)
+    def observation(self, obs):
         # pylint: disable=no-member
 
         if self.grayscale:
-            frame = np.expand_dims(frame, -1)
+            obs = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+        obs = cv2.resize(obs, (self.frame_width, self.frame_height), interpolation=cv2.INTER_AREA)
+        # pylint: disable=no-member
 
-        obs = frame
+        if self.grayscale:
+            obs = np.expand_dims(obs, -1)
+
         return obs
 
 
@@ -314,10 +312,10 @@ class ScaleFrame(gym.ObservationWrapper):
         gym.ObservationWrapper.__init__(self, env)
         self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=env.observation_space.shape, dtype=np.float32)
 
-    def observation(self, observation):
+    def observation(self, obs):
         # careful! This undoes the memory optimization, use
         # with smaller replay buffers only.
-        return np.array(observation).astype(np.float32) / 255.0
+        return np.array(obs).astype(np.float32) / 255.0
 
 
 class VisitedRoomInfo(gym.Wrapper):
@@ -354,10 +352,10 @@ class ObscureObservation(gym.ObservationWrapper):
             raise ValueError(f'Expect obscure epsilon should be between [0.0, 1), got {epsilon}')
         self._eps = epsilon
 
-    def observation(self, observation):
+    def observation(self, obs):
         if self.env.unwrapped.np_random.random() <= self._eps:
-            observation = np.zeros_like(observation, dtype=self.observation_space.dtype)
-        return observation
+            obs = np.zeros_like(obs, dtype=self.observation_space.dtype)
+        return obs
 
 
 class ClipRewardWithBound(gym.RewardWrapper):
@@ -382,10 +380,10 @@ class ObservationChannelFirst(gym.ObservationWrapper):
         new_dtype = env.observation_space.dtype if not scale_obs else np.float32
         self.observation_space = Box(low=_low, high=_high, shape=new_shape, dtype=new_dtype)
 
-    def observation(self, observation):
+    def observation(self, obs):
         # permute [H, W, C] array to in the range [C, H, W]
         # return np.transpose(observation, axes=(2, 0, 1)).astype(self.observation_space.dtype)
-        obs = np.asarray(observation, dtype=self.observation_space.dtype).transpose(2, 0, 1)
+        obs = np.asarray(obs, dtype=self.observation_space.dtype).transpose(2, 0, 1)
         # make sure it's C-contiguous for compress state
         return np.ascontiguousarray(obs, dtype=self.observation_space.dtype)
 
@@ -393,8 +391,8 @@ class ObservationChannelFirst(gym.ObservationWrapper):
 class ObservationToNumpy(gym.ObservationWrapper):
     """Make the observation into numpy ndarrays."""
 
-    def observation(self, observation):
-        return np.asarray(observation, dtype=self.observation_space.dtype)
+    def observation(self, obs):
+        return np.asarray(obs, dtype=self.observation_space.dtype)
 
 
 class ClipObservationWithBound(gym.ObservationWrapper):
@@ -404,8 +402,8 @@ class ClipObservationWithBound(gym.ObservationWrapper):
         super().__init__(env)
         self._max_abs_value = max_abs_value
 
-    def observation(self, observation):
-        return np.clip(observation, -self._max_abs_value, self._max_abs_value)
+    def observation(self, obs):
+        return np.clip(obs, -self._max_abs_value, self._max_abs_value)
 
 
 class RecordRawReward(gym.Wrapper):

deep_rl_zoo/ngu/agent.py
@@ -108,6 +108,7 @@ class Actor(types_lib.Agent):
         num_policies: int,
         policy_beta: float,
         episodic_memory_capacity: int,
+        reset_episodic_memory: bool,
         num_neighbors: int,
         cluster_distance: float,
         kernel_epsilon: float,
@@ -134,6 +135,7 @@ class Actor(types_lib.Agent):
             num_policies: number of exploring and exploiting policies.
             policy_beta: intrinsic reward scale beta.
             episodic_memory_capacity: maximum capacity of episodic memory.
+            reset_episodic_memory: Reset the episodic_memory on every episode.
             num_neighbors: number of K-NN neighbors for compute episodic bonus.
             cluster_distance: K-NN neighbors cluster distance for compute episodic bonus.
             kernel_epsilon: K-NN kernel epsilon for compute episodic bonus.
@@ -217,6 +219,8 @@ class Actor(types_lib.Agent):
         self._policy_discount = None
         self._sample_policy()
 
+        self._reset_episodic_memory = reset_episodic_memory
+
         # E-greedy policy epsilon, rank 0 has the lowest noise, while rank N-1 has the highest noise.
         epsilons = distributed.get_actor_exploration_epsilon(num_actors)
         self._exploration_epsilon = epsilons[self.rank]
@@ -253,7 +257,7 @@ class Actor(types_lib.Agent):
         self._step_t += 1
 
         if self._step_t % self._actor_update_q_network_interval == 0:
-            self._update_q_network_actor_network(False)
+            self._update_actor_network(False)
 
         q_t, a_t, prob_a_t, hidden_s = self.act(timestep)
 
@@ -295,9 +299,17 @@ class Actor(types_lib.Agent):
         """This method should be called at the beginning of every episode before take any action."""
         self._unroll.reset()
 
-        self._episodic_module.reset()  # Is this necessary?
+        # From NGU Paper on MONTEZUMA’S REVENGE:
+        """
+        Instead of resetting the memory after every episode, we do it after a small number of
+        consecutive episodes, which we call a meta-episode. This structure plays an important role when the
+        agent faces irreversible choices.
+        """
 
-        self._update_q_network_actor_network(True)
+        if self._reset_episodic_memory:
+            self._episodic_module.reset()
+
+        self._update_actor_network(True)
 
         self._sample_policy()
 
@@ -355,7 +367,7 @@ class Actor(types_lib.Agent):
         # Important note, store hidden states for every step in the unroll will consume HUGE memory.
         self._queue.put(unrolled_transition)
 
-    def _update_q_network_actor_network(self, update_embed: bool = False):
+    def _update_actor_network(self, update_embed: bool = False):
         q_state_dict = self._shared_params['network']
         embed_state_dict = self._shared_params['embedding_network']
         rnd_state_dict = self._shared_params['rnd_predictor_network']
@@ -563,7 +575,7 @@ class Learner(types_lib.Learner):
 
         # Copy Q network parameters to target Q network, every m updates
         if self._update_t > 1 and self._update_t % self._target_net_update_interval == 0:
-            self._update_q_network_target_network()
+            self._update_target_network()
 
     def _update_q_network(self, transitions: NguTransition, weights: np.ndarray) -> np.ndarray:
         """Update online Q network."""
@@ -677,7 +689,7 @@ class Learner(types_lib.Learner):
         discount_t = (~done).float() * discount  # (T+1, B)
 
         # Derive target policy probabilities from q values.
-        target_policy_probs = F.softmax(q_t, dim=-1)  # [T+1, B, action_dim]
+        target_policy_probs = F.softmax(target_q_t, dim=-1)  # [T+1, B, action_dim]
 
         if self._transformed_retrace:
             transform_tx_pair = nonlinear_bellman.SIGNED_HYPERBOLIC_PAIR
@@ -690,8 +702,8 @@ class Learner(types_lib.Learner):
             q_t=target_q_t[1:],
             a_tm1=a_t[:-1],
             a_t=a_t[1:],
-            r_t=r_t[:-1],
-            discount_t=discount_t[:-1],
+            r_t=r_t[1:],
+            discount_t=discount_t[1:],
             pi_t=target_policy_probs[1:],
             mu_t=behavior_prob_a_t[1:],
             lambda_=self._retrace_lambda,
@@ -861,7 +873,7 @@ class Learner(types_lib.Learner):
 
         return (init_h, init_c)
 
-    def _update_q_network_target_network(self):
+    def _update_target_network(self):
         self._target_network.load_state_dict(self._network.state_dict())
         self._target_update_t += 1
 